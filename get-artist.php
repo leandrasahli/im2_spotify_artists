c3f40799cb1f15c44bd6ed2dc9c96131b9768024
leandrasahli <leandra.sahli@stud.fhgr.ch>
2026-05-26 13:27:59 +0200

    Rohversion (alle php etc. entfernt)

--- get-artist.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-$_GET['action'] = 'artist';
-
-$_GET['id'] = $_GET['id'] ?? '06HL4z0CvFAxyc27GXpf02';
-
-require __DIR__ . '/spotify-api.php';
--- get-genres.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-$_GET['action'] = 'genres';
-
-require __DIR__ . '/spotify-api.php';
